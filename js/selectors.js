67d499b5bc9e1479fdd0dda2509196757b4cd0c3
Samantha Sconyers <samanthasconyers@gmail.com>
2018-06-26 22:19:21 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -8,13 +8,13 @@ function lastImageSelector() {
 }
 
 function ninjaBabySelector() {
-  return $('#baby-ninja')
+  return $('#baby-ninja');
 }
 
 function divSelector() {
-  return $('.pics')
+  return $('.pics');
 }
 
 function firstListItem() {
-  return $('ul#pic-list li:first-child')
+  return $('ul#pic-list li:first-child');
 }
